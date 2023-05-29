1b076dc1179a96b8c3a9350acdf53f910a4a174a
Bikash Nath <72164894+b0n21en5@users.noreply.github.com>
2023-05-29 16:48:15 +0530

    sub array with given sum with O(n)

---
+++ arrays/SubArraySum.cpp
@@ -0,0 +1,53 @@
+// c++ program to find out
+// sub array with given sum
+
+// Time Complexity: O(n)
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n, s;
+    cin >> n >> s;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    int i = 0, j = 0, st = -1, en = -1, sum = 0;
+
+    while (j < n && sum + arr[j] <= s)
+    {
+        sum += arr[j];
+        j++;
+    }
+
+    if (sum == s)
+    {
+        cout << i + 1 << " " << j << endl;
+        return 0;
+    }
+
+    while (j < n)
+    {
+        sum += arr[j];
+
+        // is sum becomes greater then
+        // decrease it from first values
+        while (sum > s)
+        {
+            sum -= arr[i];
+            i++;
+        }
+
+        if (sum == s)
+        {
+            st = i + 1;
+            en = j + 1;
+            break;
+        }
+        j++;
+    }
+
+    cout << st << " " << en << endl;
+}
